0e61abb1d54a6b20011e7bd6d917f86fa471abd0
Jesse De Jong <jdoceau@gmail.com>
2021-10-25 10:25:48 +1100

    plumbed routing for each page

--- src/App.js
+++ src/App.js
@@ -7,8 +7,11 @@ import {
 } from "react-router-dom";
 // Import Components
 import Header from './components/Header';
-import Navigation from './components/Navigation';
 import Footer from './components/Footer';
+import About from './components/pages/About';
+import Portfolio from './components/pages/Portfolio';
+import Contact from './components/pages/Contact';
+import Resume from './components/pages/Resume';
 
 // 
 function App() {
@@ -16,7 +19,22 @@ function App() {
   <> 
     <Router>
       <Header />
+        <Switch>
 
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/portfolio">
+            <Portfolio />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/resume">
+            <Resume />
+          </Route>
+
+        </Switch>
       <Footer />
     </Router>
   </>
