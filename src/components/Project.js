5d5bb7efbf3ea4d919825a5ace05b4f425ac0f97
Jesse De Jong <jdoceau@gmail.com>
2021-10-25 11:47:59 +1100

    working project components for portfolio page (stock img)

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,15 +1,27 @@
 const Project = ({
-    url,
-    repoName
+    repoUrl,
+    deployedUrl,
+    repoName,
+    img
 }) => {
+    const styles = {
+        card: {
+            width: "18rem",
+            margin: "10px"
+        },
+        button: {
+            margin: "4px"
+        }
+    }
     return (
 <>
-    <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="..."></img>
-        <div class="card-body">
-            <h5 class="card-title">{repoName}</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href={url} class="btn btn-primary">View Repo</a>
+    <div className="card" style={styles.card}>
+        <img src={img} className="card-img-top" alt={repoName}></img>
+        <div className="card-body">
+            <h5 className="card-title">{repoName}</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href={repoUrl} className="btn btn-primary" style={styles.button}>View Repo</a>
+            <a href={deployedUrl} className="btn btn-info" style={styles.button}>View Deployed</a>
         </div>
     </div>
 </>
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,37 +1,44 @@
 import Project from "../Project";
 
 export default function Portfolio() {
-  
+  const tempImg = "https://picsum.photos/200";
+
   const repos = {
     a: {
-      url: "https://github.com/Jesse-DeJong/react-portfolio",
+      repoUrl: "https://github.com/Jesse-DeJong/react-portfolio",
+      deployedUrl: "",
       repoName: "React Portfolio",
-      img: ""
+      img: tempImg
     },
     b: {
-      url: "https://github.com/Jesse-DeJong/indexedDB-budget-tracker",
+      repoUrl: "https://github.com/Jesse-DeJong/indexedDB-budget-tracker",
+      deployedUrl: "",
       repoName: "On/Off-line Budget Tracker",
-      img: ""
+      img: tempImg
     },
     c: {
-      url: "https://github.com/Jesse-DeJong/workout-tracker",
+      repoUrl: "https://github.com/Jesse-DeJong/workout-tracker",
+      deployedUrl: "",
       repoName: "Workout Tracker",
-      img: ""
+      img: tempImg
     },
     d: {
-      url: "https://github.com/Jesse-DeJong/tech_blog",
+      repoUrl: "https://github.com/Jesse-DeJong/tech_blog",
+      deployedUrl: "",
       repoName: "Tech Blog",
-      img: ""
+      img: tempImg
     },
     e: {
-      url: "https://gist.github.com/Jesse-DeJong/ea5a2f6193dd1d9c36fda59a1dd8c2ff",
+      repoUrl: "https://gist.github.com/Jesse-DeJong/ea5a2f6193dd1d9c36fda59a1dd8c2ff",
+      deployedUrl: "",
       repoName: "Reg-exPLAINED",
-      img: ""
+      img: tempImg
     },
     f: {
-      url: "https://github.com/Jesse-DeJong/Weather-App",
+      repoUrl: "https://github.com/Jesse-DeJong/Weather-App",
+      deployedUrl: "",
       repoName: "Weather Dashboard",
-      img: ""
+      img: tempImg
     }
   }
 
@@ -39,11 +46,46 @@ export default function Portfolio() {
     <div>
       <h1>Portfolio Page</h1>
 
+    <div className="container-fluid d-flex justify-content-evenly">
       <Project 
-        url={repos.a.url}
+        repoUrl={repos.a.repoUrl}
+        deployedUrl={repos.a.deployedUrl}
         repoName={repos.a.repoName} 
         img={repos.a.img}
       />
+      <Project 
+        repoUrl={repos.b.repoUrl}
+        deployedUrl={repos.b.deployedUrl}
+        repoName={repos.b.repoName} 
+        img={repos.b.img}
+      />
+      <Project 
+        repoUrl={repos.c.repoUrl}
+        deployedUrl={repos.c.deployedUrl}
+        repoName={repos.c.repoName} 
+        img={repos.c.img}
+      />
+    </div>
+    <div className="container-fluid d-flex justify-content-evenly">
+      <Project 
+        repoUrl={repos.d.repoUrl}
+        deployedUrl={repos.d.deployedUrl}
+        repoName={repos.d.repoName} 
+        img={repos.d.img}
+      />
+      <Project 
+        repoUrl={repos.e.repoUrl}
+        deployedUrl={repos.e.deployedUrl}
+        repoName={repos.e.repoName} 
+        img={repos.e.img}
+      />
+      <Project 
+        repoUrl={repos.f.repoUrl}
+        deployedUrl={repos.f.deployedUrl}
+        repoName={repos.f.repoName} 
+        img={repos.f.img}
+      />
+    </div>
   
     </div>
   );
